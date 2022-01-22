7ad8a1dda75b9d3f6a43094c1ecea58dd16455c9
princeprince <sistercharmings@gmail.com>
2022-01-22 13:04:31 +0000

    changed a file in the settings.py in the casual app

--- casual/settings.py
+++ casual/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e4r6#^fs@e5srqel7bvo$0u=)bd)g3*oulmw&ka^#nz^96kavu'
+SECRET_KEY = '*********************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
